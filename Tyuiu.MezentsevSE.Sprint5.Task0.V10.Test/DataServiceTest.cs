f32751211c85feb6f2479eef1b975490815c6e4e
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-11-19 17:53:13 +0500

    Add project files.

---
+++ Tyuiu.MezentsevSE.Sprint5.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.MezentsevSE.Sprint5.Task0.V10.Lib;
+namespace Tyuiu.MezentsevSE.Sprint5.Task0.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.MezentsevSE.Sprint5\Tyuiu.MezentsevSE.Sprint5.Task0.V10\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.MezentsevSE.Sprint5.Task0.V10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MezentsevSE.Sprint5.Task0.V10.Lib;
+namespace Tyuiu.MezentsevSE.Sprint5.Task0.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 2;
+
+            Console.Title = "Спринт #5| Выполнил: Мезенцев С.Е. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Запись данных в текстовый файл                                    *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил Мезенцев Семён Евгеньевич| ИИПб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение выражения , результат сохранить в текстовый файл     *");
+            
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
