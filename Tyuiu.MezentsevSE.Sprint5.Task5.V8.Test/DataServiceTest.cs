ae55fa0b2f3af05d4c6892d9c6d0b2ea34727eff
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-11-26 17:44:57 +0500

    Task5

---
+++ Tyuiu.MezentsevSE.Sprint5.Task5.V8.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.MezentsevSE.Sprint5.Task5.V8.Lib;
+namespace Tyuiu.MezentsevSE.Sprint5.Task5.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V8.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.MezentsevSE.Sprint5.Task5.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MezentsevSE.Sprint5.Task5.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MezentsevSE.Sprint5.Task5.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42e23da0-e6fa-4c53-b2fc-8b4e3864d791")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
