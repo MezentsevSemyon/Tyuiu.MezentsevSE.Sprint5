bb44884f5284d41718db006253bfed6050631396
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-11-26 18:25:12 +0500

    Task6

---
+++ Tyuiu.MezentsevSE.Sprint5.Task6.V16.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.MezentsevSE.Sprint5.Task6.V16.Lib
+{
+    public class DataService : ISprint5Task6V16
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if ((line[i] >= 'A') && (line[i] <= 'z'))
+                        {
+                            if ((line[i + 1] == ' ') || (line[i + 1] == '.') || (line[i + 1] == ','))
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.MezentsevSE.Sprint5.Task6.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MezentsevSE.Sprint5.Task6.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MezentsevSE.Sprint5.Task6.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba08ccf7-652d-43c4-8f01-aad9d3ed92bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
